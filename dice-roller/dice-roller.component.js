2867d2e6dc2a6a35bcdf4732981dad606d84525a
Eli Cecil <hello@elicecil.art>
2018-03-15 20:26:26 -0400

    Dice rolls now add together

--- dice-roller/dice-roller.component.js
+++ dice-roller/dice-roller.component.js
@@ -2,14 +2,9 @@ angular.module("dndToolKit").component("diceRoller", {
     templateUrl: "dice-roller/dice-roller.html",
   
     controller: ["DiceService", function(DiceService) {
-        this.roll = ""
 
         this.rollStats = function() {
-            var roll = DiceService.rollStats();
-            var min = Math.min(roll);
-            var finalRoll = roll.filter(e => e != min);
-            console.log(min);
-
+            DiceService.addStats();
         }
     }]
 });
--- dice-roller/dice-roller.service.js
+++ dice-roller/dice-roller.service.js
@@ -1,10 +1,19 @@
 angular.module("dndToolKit").service("DiceService", function() {
-    this.rolls = [];
-    
-    this.rollStats = function() {
-        for(var i = 0; i < 4; i++) {
-            var roll = Math.floor(Math.random() * 7);
-            return (roll);
-        }
+    this.rollDie = function() {
+        return Math.ceil(Math.random() * 4);
     } 
+
+    this.rollDice = () => {
+        return Array(3).fill(0).map(this.rollDie);
+    }
+
+    this.addStats = function() {
+        var dice = this.rollDice();
+        return this.rollDice().reduce((total, amount) => total + amount);
+    }
+    // this.filterLowest = () => {
+    //     var dice = this.rollDice();
+    //     var min = Math.min(...dice);
+    //     return dice.filter(roll => roll != min);
+    // }
 });
