cc88ccced91cd318ce52d364628d755f26dbb479
Eli Cecil <hello@elicecil.art>
2018-03-17 10:38:04 -0400

    Tightened up functionality of dice roller to include more authentic DnD rolls

--- dice-roller/dice-roller.service.js
+++ dice-roller/dice-roller.service.js
@@ -1,19 +1,36 @@
 angular.module("dndToolKit").service("DiceService", function() {
+    this.rolls = [];
+
     this.rollDie = function() {
-        return Math.ceil(Math.random() * 4);
+        return Math.ceil(Math.random() * 6);
     } 
 
     this.rollDice = () => {
-        return Array(3).fill(0).map(this.rollDie);
+        return Array(4).fill(0).map(this.rollDie);
+    }
+
+    this.findLowest = () => {
+        var dice = this.rollDice();
+        var min = Math.min(...dice);
+        var indexOfMin = dice.indexOf(min);
+        dice[indexOfMin] = 0;
+        return dice;
     }
 
     this.addStats = function() {
-        var dice = this.rollDice();
-        return this.rollDice().reduce((total, amount) => total + amount);
+        for(var i = 0; i < 6; i++) {
+            var dice = this.rollDice();
+            dice = this.findLowest(dice);
+            var sum = this.rollDice().reduce((total, amount) => total + amount);
+            console.log(dice)
+            if(sum >= 8) {
+                this.rolls.push(sum)
+                console.log(this.rolls);
+            } else {
+                sum = 8;
+                this.rolls.push(sum)
+                console.log(this.rolls);
+            }
+        }
     }
-    // this.filterLowest = () => {
-    //     var dice = this.rollDice();
-    //     var min = Math.min(...dice);
-    //     return dice.filter(roll => roll != min);
-    // }
 });
