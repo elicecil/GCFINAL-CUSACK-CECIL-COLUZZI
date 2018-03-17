af480bb2d3321562a33d5e7d8fe2e3f2c4a7b24d
Eli Cecil <hello@elicecil.art>
2018-03-17 15:11:50 -0400

    Dice roller now runs on click when choosing race

--- dice-roller/dice-roller.component.js
+++ dice-roller/dice-roller.component.js
@@ -2,21 +2,7 @@ angular.module("dndToolKit").component("diceRoller", {
     templateUrl: "dice-roller/dice-roller.html",
   
     controller: ["DiceService", "CharacterService", function(DiceService, CharacterService) {
-        this.bonuses = [];
-        this.addedStats = [];
-        this.rollStats = function() {
-            DiceService.addStats();
-        }
-
-        this.addBonuses = function() {
-            this.bonuses = CharacterService.character.race.ability_bonuses;
-            var dice = DiceService.addStats();
-            console.log("Dice", dice)
-            console.log("Bonus", this.bonuses);
-            for(var i = 0; i < 6; i++) {
-                this.addedStats.push(this.bonuses[i] + dice[i]);
-                console.log("Final", this.addedStats);
-            }
-        }
+        
+    
     }]
 });
